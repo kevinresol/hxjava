0ec5c0fb3b4651f0467e19191f2cda5589676681
Kevin Leung <kevinresol@gmail.com>
2021-09-14 20:56:35 +0800

    Fix for JDK9+

--- jar-tool/Main.hx
+++ jar-tool/Main.hx
@@ -11,7 +11,7 @@ import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes.Opcodes_Statics;
+import org.objectweb.asm.Opcodes;
 import java.Lib.println;
 
 using StringTools;
@@ -19,44 +19,53 @@ using StringTools;
 /**
  * HOW TO USE:
  *
- * 1) Unzip JAR file into some folder. For example "jar_content";
+ * 1) Unpack OpenJDK standard lib bytecode (.class files) into some folder. For example "jar_content";
+ *   a) If JDK <= 8: unzip rt.jar
+ *   b) If JDK >= 9: bin/jimage lib/modules
  * 2) Run `java -jar Main.jar jar_content`;
  * 3) Pick up generated `hxjava-std.jar`.
  */
 final class Main {
+	
 	static function main():Void {
-		function process(dir:String) {
-			if (FileSystem.exists(dir)) {
-				for (file in FileSystem.readDirectory(dir)) {
-					final path = Path.join([dir, file]);
-					if (!FileSystem.isDirectory(path)) {
-						if (path.endsWith(".class")) {
-							println('Process $path... ');
-							final obj:NativeArray<Int8> = Files.readAllBytes(new File(path).toPath());
-							final reader = new ClassReader(obj);
-							final writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
-							final visitor = new CustomClassVisitor(writer);
-
-							reader.accept(visitor, 0);
-
-							final stream = new FileOutputStream(path);
-							stream.write(writer.toByteArray());
-						}
-					} else {
-						process(Path.addTrailingSlash(path));
-					}
-				}
-			} else {
-				println('Wrong path. $dir is not exists');
-			}
-		}
 
 		final args:Array<String> = Sys.args();
 		if (args.length > 0) {
-			final dir = args[0];
-			process(dir);
+			final root = Path.addTrailingSlash(args[0]);
+			final out = Path.join([root, '../interim']);
+			
+			(function process(dir:String) {
+				if (FileSystem.exists(dir)) {
+					for (file in FileSystem.readDirectory(dir)) {
+						final path = Path.join([dir, file]);
+						if (!FileSystem.isDirectory(path)) {
+							if (path.endsWith(".class") && Path.withoutDirectory(path) != 'module-info.class') {
+								println('Process $path... ');
+								final obj:NativeArray<Int8> = Files.readAllBytes(new File(path).toPath());
+								final reader = new ClassReader(obj);
+								final writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+								final visitor = new CustomClassVisitor(writer);
+
+								reader.accept(visitor, 0);
+
+								final parts = path.substr(root.length).split('/');
+								final dst = Path.join([out].concat(parts[0].contains('.') ? parts.slice(1) : parts));
+								if(!FileSystem.exists(Path.directory(dst))) 
+									FileSystem.createDirectory(Path.directory(dst));
+								final stream = new FileOutputStream(dst);
+								stream.write(writer.toByteArray());
+							}
+						} else {
+							process(Path.addTrailingSlash(path));
+						}
+					}
+				} else {
+					println('Wrong path. $dir is not exists');
+				}
+			})(root);
+			
 			println("\nPacking jar...\n");
-			if (Sys.command('jar', ["cvf", "hxjava-std.jar", "-C", dir, "."]) == 0) {
+			if (Sys.command('jar', ["cvf", "hxjava-std.jar", "-C", out, "."]) == 0) {
 				println("\nDone!\n");
 			} else {
 				println("Oops, something went wrong.");
@@ -69,7 +78,7 @@ final class Main {
 class CustomClassVisitor extends ClassVisitor {
 
 	public function new(visitor:ClassVisitor) {
-		super(Opcodes_Statics.ASM7, visitor);
+		super(Opcodes.ASM7, visitor);
 	}
 
 	@:overload
@@ -95,7 +104,7 @@ class CustomMethodVisitor extends MethodVisitor {
 	final target:MethodVisitor;
 
 	public function new(target:MethodVisitor) {
-		super(Opcodes_Statics.ASM7, null);
+		super(Opcodes.ASM7, null);
 		this.target = target;
 	}
 
